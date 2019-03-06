63326bb424c17055f605e2cca0f83d51f9afc322
Taichi <taichi@curipha.jp>
2019-03-07 01:08:27 +0900

    Fix linter warnings

--- uixaml.py
+++ uixaml.py
@@ -1,23 +1,29 @@
 # XML namespaces
 xamlns = {
-  'xaml': 'http://schemas.microsoft.com/netfx/2009/xaml/activities', # Default namespace
+  'xaml': 'http://schemas.microsoft.com/netfx/2009/xaml/activities',  # Default namespace
   'sap2010': 'http://schemas.microsoft.com/netfx/2010/xaml/activities/presentation',
   'ui': 'http://schemas.uipath.com/workflow/activities',
   'x': 'http://schemas.microsoft.com/winfx/2006/xaml',
 }
 
 # Set of Workbook activities (as of 18.4.3)
-wbactivities = {'ui:AppendRange', 'ui:GetTableRange', 'ui:ReadCell', 'ui:ReadCellFormula', 'ui:ReadColumn', 'ui:ReadRange', 'ui:ReadRow', 'ui:WriteCell', 'ui:WriteRange'}
+wbactivities = {
+  'ui:AppendRange', 'ui:GetTableRange', 'ui:ReadCell', 'ui:ReadCellFormula', 'ui:ReadColumn',
+  'ui:ReadRange', 'ui:ReadRow', 'ui:WriteCell', 'ui:WriteRange',
+}
 
 # Set of Open/Attach Scopes (as of 18.4.3)
-wndscopes = {'ui:ElementScope', 'ui:WindowScope', 'ui:BrowserScope', 'ui:OpenApplication', 'ui:OpenBrowser'}
+wndscopes = {
+  'ui:ElementScope', 'ui:WindowScope', 'ui:BrowserScope', 'ui:OpenApplication', 'ui:OpenBrowser',
+}
 
 # Set of Special keys (as of 18.4.3)
 specialkey = {
-  'add', 'alt', 'lalt', 'ralt', 'back', 'break', 'caps', 'ctrl', 'lctrl', 'rctrl', 'decimal', 'del', 'div', 'down', 'end', 'enter', 'numEnter', 'esc',
-  'f1', 'f2', 'f3', 'f4', 'f5', 'f6', 'f7', 'f8', 'f9', 'f10', 'f11', 'f12', 'home', 'ins', 'left', 'mul', 'num',
-  'num0', 'num1', 'num2', 'num3', 'num4', 'num5', 'num6', 'num7', 'num8', 'num9',
-  'pause', 'pgup', 'pgdn', 'right', 'scroll', 'shift', 'lshift', 'rshift', 'sleep', 'sub', 'tab', 'up'
+  'add', 'alt', 'lalt', 'ralt', 'back', 'break', 'caps', 'ctrl', 'lctrl', 'rctrl', 'decimal',
+  'del', 'div', 'down', 'end', 'enter', 'numEnter', 'esc', 'f1', 'f2', 'f3', 'f4', 'f5', 'f6',
+  'f7', 'f8', 'f9', 'f10', 'f11', 'f12', 'home', 'ins', 'left', 'mul', 'num', 'num0', 'num1',
+  'num2', 'num3', 'num4', 'num5', 'num6', 'num7', 'num8', 'num9', 'pause', 'pgup', 'pgdn', 'right',
+  'scroll', 'shift', 'lshift', 'rshift', 'sleep', 'sub', 'tab', 'up',
 }
 
 
